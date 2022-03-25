d2bf3529bf1c8ee3d7d4ae255b9841fb34ec5eb3
FedericaFloris <feflo92@gmail.com>
2022-03-25 15:30:09 +0100

    ultimo

---
+++ JavaScript.js
@@ -0,0 +1,5 @@
+﻿function Salva() {
+    let key = document.getElementById("").value; //il testo che metto nell input lo metto nella key
+    let value = document.getElementById("storageValue").value; //il testo che metto nell input lo metto nel value
+    storage.setItem(key, value);
+}
